2ca59f80ffd26cdc9989ac3c26dcc8955e19bb45
theone <297190869@qq.com>
2016-03-16 21:03:50 +0800

    view modify

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,39 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (err, req, res, next) {
+  var meta = '[' + new Date() + '] ' + req.url + '\n';
+  errorLog.write(meta + err.stack + '\n');
+  next();
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (app) {
 
-module.exports = router;
+  /* GET home page. */
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: '主页' });
+  });
+
+  app.get('/reg', function (req, res, next) {
+    res.render('reg', { title: '注册'})
+  });
+
+  app.post('/reg', function (req, res, next) {
+
+  });
+
+  app.get('/login', function (req, res, next) {
+    res.render('login', { title: '登陆'});
+  });
+
+  app.post('/login', function (req, res, next) {
+
+  });
+
+  app.get('/post', function (req, res) {
+    res.render('post',{title: '发表'});
+  });
+
+
+}
